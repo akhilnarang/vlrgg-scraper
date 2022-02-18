a0df2ed626248bed7be0855b27339bfd975d499a
Akhil Narang <me@akhilnarang.dev>
2022-02-18 21:26:51 +0530

    feat(app): enable GzipMiddleware
    These responses are huge, so this helps a lot
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from starlette.middleware.gzip import GZipMiddleware
 
 from app.api.v1.api import router
 
 app = FastAPI()
+app.add_middleware(GZipMiddleware)
 app.include_router(router, prefix="/api/v1")
