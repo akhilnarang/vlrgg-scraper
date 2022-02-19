2f913caa791edd50e1f77bf0a2f46e4b90cdb9a7
Akhil Narang <me@akhilnarang.dev>
2022-02-19 21:01:45 +0530

    Set title and description for docs
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/main.py
+++ app/main.py
@@ -3,6 +3,6 @@ from starlette.middleware.gzip import GZipMiddleware
 
 from app.api.v1.api import router
 
-app = FastAPI()
+app = FastAPI(title="Scraper", description="Scraper for VLR.gg that exposes a REST API for some data available there")
 app.add_middleware(GZipMiddleware)
 app.include_router(router, prefix="/api/v1")
