c539ad299c4cf4bd4ec8901777edf68449b28174
Akhil Narang <me@akhilnarang.dev>
2022-02-19 21:11:47 +0530

    Set timezone to UTC to make sure time values are correctly returned
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 from fastapi import FastAPI
 from starlette.middleware.gzip import GZipMiddleware
 
@@ -6,3 +9,7 @@ from app.api.v1.api import router
 app = FastAPI(title="Scraper", description="Scraper for VLR.gg that exposes a REST API for some data available there")
 app.add_middleware(GZipMiddleware)
 app.include_router(router, prefix="/api/v1")
+
+# Ensure that our timezone is set to UTC to make sure that time values are returned correctly
+os.environ["TZ"] = "UTC"
+time.tzset()
