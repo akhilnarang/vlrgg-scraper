efc5b28713de4d5d3bd1122c3216b5d79b162bc7
Akhil Narang <me@akhilnarang.dev>
2022-03-03 15:37:04 +0530

    feat(auth): allow not setting a secret key
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    SECRET_KEY: str
+    SECRET_KEY: str | None
 
     class Config:
         env_file = ".env"
--- app/main.py
+++ app/main.py
@@ -6,10 +6,15 @@ from starlette.middleware.gzip import GZipMiddleware
 
 from app.api import deps
 from app.api.v1.api import router
+from app.core.config import settings
 
 app = FastAPI(title="Scraper", description="Scraper for VLR.gg that exposes a REST API for some data available there")
 app.add_middleware(GZipMiddleware)
-app.include_router(router, prefix="/api/v1", dependencies=[Depends(deps.verify_token)])
+
+if settings.SECRET_KEY:
+    app.include_router(router, prefix="/api/v1", dependencies=[Depends(deps.verify_token)])
+else:
+    app.include_router(router, prefix="/api/v1")
 
 # Ensure that our timezone is set to UTC to make sure that time values are returned correctly
 os.environ["TZ"] = "UTC"
