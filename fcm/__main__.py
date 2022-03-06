e73d457d31ee46c08163b5a4f31049f368c4c73c
Akhil Narang <me@akhilnarang.dev>
2022-03-06 12:03:32 +0530

    refactor(fcm): cleanup code, call service function instead of abusing TestClient
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- fcm/__main__.py
+++ fcm/__main__.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from .main import send
 
-send()
+asyncio.run(send())
--- fcm/main.py
+++ fcm/main.py
@@ -2,46 +2,44 @@ import os
 import sys
 from datetime import datetime
 
-from fastapi.testclient import TestClient
 from firebase_admin import initialize_app, messaging
 
-from app.core.config import settings
-from app.main import app
+from app.constants import MatchStatus
+from app.services import matches
 
 
-def send():
+async def send():
     if os.getenv("GOOGLE_APPLICATION_CREDENTIALS") is None:
         print(
-            "Please set the environment variable `GOOGLE_APPLICATION_CREDENTIALS` to the path of the service account JSON"
+            "Please set the environment variable `GOOGLE_APPLICATION_CREDENTIALS` to the path of the service account "
+            "JSON "
         )
         sys.exit(1)
 
-    client = TestClient(app)
     current_time = datetime.utcnow()
-    headers = {}
-    if settings.SECRET_KEY:
-        headers["Authorization"] = f"Bearer {settings.SECRET_KEY}"
     upcoming_matches = [
         match
-        for match in client.get("/api/v1/matches", headers=headers).json()
-        if match["status"].lower() == "upcoming"
-        and (datetime.fromisoformat(match["time"]) - current_time).total_seconds() < 900
+        for match in await matches.get_upcoming_matches()
+        if match.status == MatchStatus.UPCOMING and (match.time - current_time).total_seconds() < 900
     ]
 
     initialize_app()
     messages = []
     for match in upcoming_matches:
         print(f"Sending notification for {match=}")
-        time_to_start = int((datetime.fromisoformat(match["time"]) - current_time).total_seconds() // 60)
+        time_to_start = int((match.time - current_time).total_seconds() // 60)
         messages.append(
             messaging.Message(
                 data={
-                    "title": f"{match['team1']['name']} vs {match['team2']['name']}",
+                    "title": f"{match.team1.name} vs {match.team2.name}",
                     "body": f"Match is starting in {time_to_start} minutes",
-                    "match_id": match["id"],
+                    "match_id": match.id,
                 },
-                topic=f"match-{match['id']}",
+                topic=f"match-{match.id}",
             )
         )
-    response = messaging.send_all(messages)
-    print(f"{vars(response)=}")
+    if messages:
+        response = messaging.send_all(messages)
+        print(f"{vars(response)=}")
+    else:
+        print("No notifications to send")
