088c3c0ccc9586e6484099e6e8fe3c52928ec519
Akhil Narang <me@akhilnarang.dev>
2022-07-09 00:39:16 +0530

    Add in optional brotli compression
    Stuck with the defaults for now, need to tweak the quality
    
    Just need to pass `Accept-Encoding: br` for brotli
    `Accept-Encoding: gzip` continues to work, as does uncompressed
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 from fastapi import Depends, FastAPI
-from starlette.middleware.gzip import GZipMiddleware
-
+from brotli_asgi import BrotliMiddleware
 from app.api import deps
 from app.api.v1.api import router
 from app.core.config import settings
 
 app = FastAPI(title="Scraper", description="Scraper for VLR.gg that exposes a REST API for some data available there")
-app.add_middleware(GZipMiddleware)
+app.add_middleware(BrotliMiddleware)
 
 if settings.SECRET_KEY:
     app.include_router(router, prefix="/api/v1", dependencies=[Depends(deps.verify_token)])
