4994a4f2bbc6c95f9d633d99a14709f8efcf117f
Akhil Narang <me@akhilnarang.dev>
2022-08-27 12:44:20 +0530

    feat(rankings): return the older way for older app versions
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/api/v1/endpoints/rankings.py
+++ app/api/v1/endpoints/rankings.py
@@ -1,7 +1,9 @@
 import json
+import logging
 from typing import Any
 
-from fastapi import APIRouter
+import semver
+from fastapi import APIRouter, Header
 
 from app import cache, schemas
 from app.services import rankings
@@ -10,8 +12,21 @@ router = APIRouter()
 
 
 @router.get("/", response_model=list[schemas.Ranking])
-async def get_rankings() -> Any:
+async def get_rankings(app_version: str | None = Header(None)) -> Any:
     try:
-        return [schemas.Ranking.parse_obj(ranking) for ranking in json.loads(await cache.get("rankings"))]
+        response = [schemas.Ranking.parse_obj(ranking) for ranking in json.loads(await cache.get("rankings"))]
     except cache.CacheMiss:
-        return await rankings.ranking_list()
+        response = await rankings.ranking_list()
+
+    # TODO: revert after a month or so
+    if app_version and semver.compare("0.2.12", app_version[1:]) > -1:
+        ranking_replace = {
+            "Asia-Pacific": "Asia Pacific",
+            "Latin America South": "Latin America - South",
+            "Latin America North": "Latin America - North",
+        }
+        for ranking in response:
+            if region := ranking_replace.get(ranking.region):
+                ranking.region = region
+
+    return response
