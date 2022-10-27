3c6d6e1cca407ad7ce8049cd65e501a27a823cae
Akhil Narang <me@akhilnarang.dev>
2022-10-27 12:30:43 +0530

    feat: add in a dockerignore file
    Also update setup script - hiredis needs a bunch of stuff
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+set -euxo pipefail
+export DEBIAN_FRONTEND=noninteractive
+apt update --yes
+apt install build-essential --yes
 pip --no-cache-dir install poetry
 poetry config virtualenvs.create false
 poetry install
 rm -rf $(poetry config cache-dir)
+apt autoremove build-essential --yes
+rm -rf /var/apt/lib/lists
