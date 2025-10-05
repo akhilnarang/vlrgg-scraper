5fbd3ac0649416d891c48c41b453b64ebc1acdd8
Akhil Narang <me@akhilnarang.dev>
2025-10-05 17:24:42 +0530

    feat: internal endpoint to download DB
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/api/v1/endpoints/internal.py
+++ app/api/v1/endpoints/internal.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
+from fastapi.responses import FileResponse
 from redis.asyncio import Redis
 
-from app.api.deps import get_redis_client
+from app.api.deps import get_redis_client, verify_internal_token
 from app.schemas import TeamCache
 
 router = APIRouter()
@@ -16,3 +17,8 @@ async def get_team_cache(client: Redis = Depends(get_redis_client)) -> list[Team
 async def set_team_cache(teams: list[TeamCache], client: Redis = Depends(get_redis_client)) -> list[TeamCache]:
     await client.hset("team", mapping={team.name: team.id for team in teams})  # type: ignore
     return [TeamCache(name=k.decode(), id=v.decode()) for k, v in (await client.hgetall("team")).items()]  # type: ignore
+
+
+@router.get("/database", dependencies=[Depends(verify_internal_token)])
+async def get_database() -> FileResponse:
+    return FileResponse(path="sqlite.db", media_type="application/vnd.sqlite3", filename="sqlite.db")
